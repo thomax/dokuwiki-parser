59b7b6560498d2347f12a95070779dfa0e2dc0fe
Thomas Drevon <thomas@sanity.io>
2019-01-07 18:25:56 +0100

    Convert dokuwiki links to actual a-hrefs. Also, fix indentation and semis

--- index.js
+++ index.js
@@ -21,11 +21,11 @@
  *                                                                   *
  *********************************************************************/
 
-'use strict';
+'use strict'
 
-exports.parse = parseFunction;
-exports.getTitle = getTitle;
-exports.getLine = getLine;
+exports.parse = parseFunction
+exports.getTitle = getTitle
+exports.getLine = getLine
 
 /**
  * This will parse the given (dokuwikiMarkdown) markdown string as dokuwiki text
@@ -34,84 +34,80 @@ exports.getLine = getLine;
  * @return {String}                  HTML string.
  */
 function parseFunction(doku) {
+  var result = []
 
-    var result = [];
+  /* Convert it to lines */
+  doku = doku.split('\n')
 
-    /* Convert it to lines */
-    doku = doku.split('\n');
+  var ulOpen = false
+  var elementToUse = 'p'
 
-    var ulOpen = false;
-    var elementToUse = 'p';
-
-    doku.forEach(function (line) {
-
-        if (line.trim().length === 0) {
-            return;
-        }
-        if (line.indexOf('  -') === 0) {
-            if (!ulOpen) {
-                result.push('<ul>');
-                elementToUse = 'li';
-                ulOpen = true;
-            }
-            line = line.substr(3).trim();
-        } else if (ulOpen) {
-            result.push('</ul>');
-            elementToUse = 'p';
-            ulOpen = false;
-        }
-        if (line.substr(0, 1) === '=') {
-            result.push(getTitle(line));
-            return;
-        }
-        result.push(getLine(line, elementToUse));
-    });
-
-    if (ulOpen) {
-        result.push('</ul>');
+  doku.forEach(function(line) {
+    if (line.trim().length === 0) {
+      return
     }
+    if (line.indexOf('  -') === 0) {
+      if (!ulOpen) {
+        result.push('<ul>')
+        elementToUse = 'li'
+        ulOpen = true
+      }
+      line = line.substr(3).trim()
+    } else if (ulOpen) {
+      result.push('</ul>')
+      elementToUse = 'p'
+      ulOpen = false
+    }
+    if (line.substr(0, 1) === '=') {
+      result.push(getTitle(line))
+      return
+    }
+    result.push(getLine(line, elementToUse))
+  })
 
-    return result.join('\n');
+  if (ulOpen) {
+    result.push('</ul>')
+  }
 
+  return result.join('\n')
 }
 
 function getTitle(line) {
-    line = line.trim();
-    var i = line.search(/[^=]/);
-    line = line.substr(i, line.length - i * 2 - 1).trim();
-    var element = 'h' + (7 - i);
-    return encapsulate(line, element);
+  line = line.trim()
+  var i = line.search(/[^=]/)
+  line = line.substr(i, line.length - i * 2 - 1).trim()
+  var element = 'h' + (7 - i)
+  return encapsulate(line, element)
 }
 
 function getLine(line, elementToUse) {
+  var matches = line.match(/\[\[[^\]]*]]/g)
+  if (matches !== null) {
+    // there are one or more links in here somewhere
+    matches.forEach(function(match) {
+      const pieces = match
+        .replace('[[', '')
+        .replace(']]', '')
+        .split(':')
 
-    var matches = line.match(/\[\[[^\]]*]]/g);
-    if (matches !== null) {
-        matches.forEach(function (match) {
+      // determine link text
+      const linkText =
+        match.indexOf('|') > -1 ? match.split('|').slice(-1)[0] : pieces[pieces.length - 1]
 
-            /* Extract the word (starts after ':' or '|' or at beginning) */
-            var i = match.lastIndexOf(':');
-            var j = match.lastIndexOf('|');
-            i = Math.max(i, j);
-            if (i === -1) {
-                i = 1;  // Just afterthe '[['
-            }
-            var newString = match.substring(i + 1);
-            newString = newString.substring(0, newString.length - 2);
+      // downcase and snakecase
+      pieces[pieces.length - 1] = pieces[pieces.length - 1].toLowerCase().replace(' ', '_')
+      const href = pieces.join('/')
+      const newString = `<a href="${href}>${linkText}</a>`
+      line = line.replace(match, newString)
+    })
+  }
 
-            /* Then replace the first result */
-            line = line.replace(match, newString);
-
-        });
-    }
-
-    return encapsulate(line.trim(), elementToUse);
+  return encapsulate(line.trim(), elementToUse)
 }
 
-
 function encapsulate(string, element) {
-    if (!element) {
-        element = 'p';
-    }
-    return '<' + element + '>' + string + '</' + element + '>';
+  if (!element) {
+    element = 'p'
+  }
+  return '<' + element + '>' + string + '</' + element + '>'
 }
